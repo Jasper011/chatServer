b5b242191d4c7735bf2a633605690914b1357cd7
Ilya <splint1777@gmail.com>
2025-02-05 16:49:45 +0300

    fix cors

--- server.js
+++ server.js
@@ -1,37 +1,75 @@
 const express = require("express");
 const http = require("http");
 const { WebSocketServer } = require("ws");
+const cors = require('cors')
 
 const app = express();
 const server = http.createServer(app);
 
 const wss = new WebSocketServer({ server });
 const rooms = new Map();
+const roomMessages = new Map();
+
+
+// const corsOptions ={
+//   origin:'*', 
+//   credentials:true,            //access-control-allow-credentials:true
+//   optionSuccessStatus:200,
+// }
+
+app.options("*", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.sendStatus(200);
+});
+
+app.use(cors({
+  origin: "*",
+  methods: "GET,POST,OPTIONS",
+  allowedHeaders: "Content-Type",
+  optionSuccessStatus: 200,
+}));
+
+app.get('/', (req, res) => {
+  res.status(200).send("1")
+})
 
 wss.on("connection", (ws) => {
   ws.currentRoom = null;
 
   ws.on("message", (message) => {
     const data = JSON.parse(message);
-    
     switch (data.type) {
       case "createRoom":
-        const roomId = data.roomId;
-        if (!rooms.has(roomId)) {
-          rooms.set(roomId, []);
+        if (!rooms.has(data.roomId)) {
+          rooms.set(data.roomId, []);
+          roomMessages.set(data.roomId, []);
         }
-        rooms.get(roomId).push(ws);
-        ws.currentRoom = roomId;
-        ws.send(JSON.stringify({ type: "roomCreated", roomId }));
+        rooms.get(data.roomId).push(ws);
+        ws.currentRoom = data.roomId;
+
+        ws.send(JSON.stringify({
+          type: "roomCreated",
+          roomId: data.roomId,
+          messages: roomMessages.get(data.roomId) || []
+        }));
+        console.log(`room ${data.roomId} created`);
+        
         break;
 
       case "joinRoom":
-        const joinRoomId = data.roomId;
-        if (rooms.has(joinRoomId)) {
+        if (rooms.has(data.roomId)) {
           if (ws.currentRoom) leaveRoom(ws);
-          rooms.get(joinRoomId).push(ws);
-          ws.currentRoom = joinRoomId;
-          ws.send(JSON.stringify({ type: "roomJoined", roomId: joinRoomId }));
+          rooms.get(data.roomId).push(ws);
+          ws.currentRoom = data.roomId;
+
+          ws.send(JSON.stringify({
+            type: "roomJoined",
+            roomId: data.roomId,
+            messages: roomMessages.get(data.roomId) || []
+          }));
+          console.log(`room ${data.roomId} joined`);
         } else {
           ws.send(JSON.stringify({ type: "error", message: "Room does not exist" }));
         }
@@ -43,19 +81,25 @@ wss.on("connection", (ws) => {
         break;
 
       case "deleteRoom":
-        const deleteRoomId = data.roomId;
-        if (rooms.has(deleteRoomId)) {
-          rooms.delete(deleteRoomId);
-          notifyAllClients(`Room "${deleteRoomId}" has been deleted.`);
-          ws.send(JSON.stringify({ type: "roomDeleted", roomId: deleteRoomId }));
+        if (rooms.has(data.roomId)) {
+          rooms.get(data.roomId).forEach(client => {
+            if (client.readyState === 1) {
+              client.send(JSON.stringify({ type: "roomDeleted", roomId: data.roomId }));
+            }
+          });
+          rooms.delete(data.roomId);
+          roomMessages.delete(data.roomId);
+          console.log(`room ${data.roomId} deleted`);
         }
         break;
 
       case "sendMessage":
-        const sendRoomId = data.roomId;
-        const messageText = data.message;
-        if (rooms.has(sendRoomId)) {
-          notifyRoomParticipants(sendRoomId, messageText, ws);
+        if (rooms.has(data.roomId)) {
+          const msg = { roomId: data.roomId, message: data.message };
+          roomMessages.get(data.roomId).push(msg);
+          console.log(`message "${data.message}" sended`);
+          
+          notifyRoomParticipants(data.roomId, msg);
         }
         break;
 
@@ -73,22 +117,14 @@ wss.on("connection", (ws) => {
   });
 });
 
-function notifyRoomParticipants(roomId, message, sender = null) {
+function notifyRoomParticipants(roomId, message) {
   if (rooms.has(roomId)) {
     rooms.get(roomId).forEach((client) => {
       if (client.readyState === 1) {
-        client.send(JSON.stringify({ type: "message", roomId, message }));
+        client.send(JSON.stringify({ type: "message", ...message }));
       }
     });
   }
-}
-
-function notifyAllClients(message) {
-  wss.clients.forEach((client) => {
-    if (client.readyState === 1) {
-      client.send(JSON.stringify({ type: "message", message }));
-    }
-  });
 }
 
 function leaveRoom(ws) {
@@ -107,12 +143,11 @@ function leaveRoom(ws) {
 function sendRoomList(ws) {
   const roomList = Array.from(rooms.entries()).map(([roomId, clients]) => ({
     roomId,
-    participants: clients.length
   }));
   ws.send(JSON.stringify({ type: "roomList", rooms: roomList }));
 }
 
 const PORT = 4000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running at port ${PORT}`);
 });
